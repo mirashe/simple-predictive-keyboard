c85a77bcd7b01bd669386a601daa5daf8c6c1040
Nabi Mirashe <nabi.mirashe@uniface.com>
2021-09-10 13:47:04 +0200

    predict_completions implemented and used for a prediction sample

--- characterwise.py
+++ characterwise.py
@@ -56,11 +56,27 @@ optimizer = RMSprop(lr=0.01)
 model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 # history = model.fit(X, y, validation_split=0.05, batch_size=128, epochs=20, shuffle=True).history
 
-# model.save('keras_model.h5')
-# pickle.dump(history, open("history.p", "wb"))
+# model.save('characterwise-model.h5')
+# pickle.dump(history, open("characterwise-history.p", "wb"))
 
-# model = load_model('keras_model.h5')
-history = pickle.load(open("history.p", "rb"))
+model = load_model('characterwise-model.h5')
+history = pickle.load(open("characterwise-history.p", "rb"))
+
+
+def predict_completions(sentence_40):
+    sentence_matrix = np.zeros((1, SEQUENCE_LENGTH, len(chars)), dtype=np.bool)
+    for t, char in enumerate(sentence_40):
+        sentence_matrix[0, t, char_indices[char]] = 1
+
+    predictions_for_first_sample = model.predict(sentence_matrix)[0]
+    number_of_guesses = 3
+    best_predictions_indices = heapq.nlargest(number_of_guesses, range(predictions_for_first_sample.size), predictions_for_first_sample.take)
+    best_predictions = np.array(chars)[best_predictions_indices]
+
+    return best_predictions
+
+
+print("Prediction sample: ", predict_completions(text[0:40]))
 
 plt.figure(figsize=(12, 5))
 plt.subplot(1, 2, 1)
@@ -69,7 +85,7 @@ plt.plot(history['val_accuracy'])
 plt.title('model accuracy')
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left');
+plt.legend(['train', 'test'], loc='upper left')
 
 plt.subplot(1, 2, 2)
 plt.plot(history['loss'])
@@ -77,5 +93,5 @@ plt.plot(history['val_loss'])
 plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left');
+plt.legend(['train', 'test'], loc='upper left')
 plt.show()
