bea1e8ee8a10e19e01a586cd73ee2293e9469e44
Nabi Mirashe <nabi.mirashe@uniface.com>
2021-10-11 02:54:39 +0200

    make it possible to accept shorter sentences

--- characterwise.py
+++ characterwise.py
@@ -2,9 +2,7 @@ import numpy as np
 import tensorflow as tf
 from keras.models import Sequential, load_model
 from keras.layers import Dense, Activation
-from keras.layers import LSTM, Dropout
-from keras.layers import TimeDistributed
-from keras.layers.core import Dense, Activation, Dropout, RepeatVector
+from keras.layers import LSTM
 from keras.optimizers import RMSprop
 import matplotlib.pyplot as plt
 import pickle
@@ -13,12 +11,10 @@ import seaborn as sns
 from pylab import rcParams
 import re
 
-# %matplotlib inline
-
 should_save = not True
-should_load = not True
+should_load = True
 input_file_path = 'uniface-code-samples-01.txt'
-model_files_title = 'characterwise-uniface-simple'
+model_files_title = 'CW-U-SO'
 
 np.random.seed(42)
 tf.random.set_seed(42)
@@ -44,23 +40,25 @@ indices_char = dict((i, c) for i, c in enumerate(chars))
 
 print(f'unique chars: {len(chars)}')
 
-SEQUENCE_LENGTH = 40
+SENTENCES_MINIMUM_LENGTH = 1
 step = 3
 sentences = []
 next_chars = []
+
+SENTENCES_EQUALIZED_LENGTH = 40
+
 for operation_index, operation_text in enumerate(operations_texts):
-    for i in range(0, len(operation_text) - SEQUENCE_LENGTH, step):
-        sentences.append(operation_text[i: i + SEQUENCE_LENGTH])
-        next_chars.append(operation_text[i + SEQUENCE_LENGTH])
+    for i in range(0, len(operation_text) - SENTENCES_MINIMUM_LENGTH, step):
+        sentences.append(operation_text[max(0, i + SENTENCES_MINIMUM_LENGTH - SENTENCES_EQUALIZED_LENGTH):
+                                        i + SENTENCES_MINIMUM_LENGTH])
+        next_chars.append(operation_text[i + SENTENCES_MINIMUM_LENGTH])
 print(f'num training examples: {len(sentences)}')
 
-sentences_equalized_length = SEQUENCE_LENGTH
-
-X = np.zeros((len(sentences), sentences_equalized_length, len(chars)), dtype=np.bool)
+X = np.zeros((len(sentences), SENTENCES_EQUALIZED_LENGTH, len(chars)), dtype=np.bool)
 y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
 for i, sentence in enumerate(sentences):
-    for t, char in enumerate(reversed(sentence[-sentences_equalized_length:])):
-        X[i, sentences_equalized_length-t-1, char_indices[char]] = 1
+    for t, char in enumerate(reversed(sentence[-SENTENCES_EQUALIZED_LENGTH:])):
+        X[i, SENTENCES_EQUALIZED_LENGTH-t-1, char_indices[char]] = 1
     y[i, char_indices[next_chars[i]]] = 1
 
 if should_load:
@@ -68,7 +66,7 @@ if should_load:
     history = pickle.load(open(model_files_title + "-history.p", "rb"))
 else:
     model = Sequential()
-    model.add(LSTM(128, input_shape=(sentences_equalized_length, len(chars))))
+    model.add(LSTM(128, input_shape=(SENTENCES_EQUALIZED_LENGTH, len(chars))))
     model.add(Dense(len(chars)))
     model.add(Activation('softmax'))
 
@@ -82,10 +80,10 @@ else:
         pickle.dump(history, open(model_files_title + '-history.p', "wb"))
 
 
-def predict_completions(sentence_40, prediction_length):
-    sentence_matrix = np.zeros((1, SEQUENCE_LENGTH, len(chars)), dtype=np.bool)
-    for t, char in enumerate(sentence_40):
-        sentence_matrix[0, t, char_indices[char]] = 1
+def predict_completions(sentence, prediction_length):
+    sentence_matrix = np.zeros((1, SENTENCES_EQUALIZED_LENGTH, len(chars)), dtype=np.bool)
+    for t, char in enumerate(reversed(sentence[-SENTENCES_EQUALIZED_LENGTH:])):
+        sentence_matrix[0, SENTENCES_EQUALIZED_LENGTH-t-1, char_indices[char]] = 1
 
     predicted_text = ""
 
@@ -100,15 +98,14 @@ def predict_completions(sentence_40, prediction_length):
         new_char_matrix = np.zeros((1, 1, len(chars)), dtype=np.bool)
         new_char_matrix[0, 0, char_indices[best_predictions[0]]] = 1
 
-        sentence_matrix = np.concatenate((sentence_matrix[0:1, 1:SEQUENCE_LENGTH, :], new_char_matrix), axis=1)
+        sentence_matrix = np.concatenate((sentence_matrix[0:1, 1:SENTENCES_EQUALIZED_LENGTH, :], new_char_matrix), axis=1)
 
     return predicted_text
 
 
-sample_start_position = 32
-input_sample = text[sample_start_position: SEQUENCE_LENGTH + sample_start_position]
+input_sample = operations_texts[3][0:40]
 print("Input sample: \r\n", input_sample)
-print("Prediction sample: ", predict_completions(input_sample, 120))
+print("Prediction sample: \r\n", predict_completions(input_sample, 120))
 
 plt.figure(figsize=(12, 5))
 plt.subplot(1, 2, 1)
